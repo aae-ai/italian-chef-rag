115e1fea52e97df7ec7a3e542802e4a27feb0f1f
aae <aaeforai@gmail.com>
2026-04-06 03:49:36 +0300

    Add gunicorn to requirements.txt and update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def chat():
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
